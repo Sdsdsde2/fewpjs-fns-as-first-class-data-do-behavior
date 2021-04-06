d57c49641c441e8ef9145fd951f27e1c16d9f794
Sdsdsde2 <imspammails31@gmail.com>
2021-04-06 15:33:16 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const HHMM = time.split(':')
+  let hours = HHMM[0]
+  let mins = HHMM[1]
+  if (parseInt(hours) <= 12) {
+    return "Good Morning"
+  }
+  else if (parseInt(hours) >= 12 && parseInt(hours) <= 17) {
+    return "Good Afternoon"
+  }
+  else if (parseInt(hours) >= 17) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let greeting = document.getElementById('greeting')
+  greeting.innerText = message;
+}
